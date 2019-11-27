f414c4892d455d75ee4529ed9ecf31ec268cff22
neoli <=798060965@qq.com>
2019-11-27 20:14:08 +0800

    build.sh

--- C++/build.sh
+++ C++/build.sh
@@ -1,15 +1,17 @@
-
-
+cd $(dirname $0)
 cd project
 rm -rf build/
 mkdir build
 cd build
 platform=$(uname)
+
 if [ "$platform" = "Linux" ]
 then
     cmake ..
+    make
+    ./a.out
 else
     cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" ..
+    make
+    ./a.exe
 fi
-make
-./a.out
