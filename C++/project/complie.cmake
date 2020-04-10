a5c441c76487e3398ae661fbf9c067882f4570d4
neoli <=798060965@qq.com>
2020-04-10 22:02:48 +0800

    update complie path

--- C++/project/complie.cmake
+++ C++/project/complie.cmake
@@ -1,6 +1,6 @@
 if(WIN32)
-    set(CMAKE_C_COMPILER "C:/MinGW/bin/gcc.exe")
-    set(CMAKE_CXX_COMPILER "C:/MinGW/bin/g++.exe")
+    set(CMAKE_C_COMPILER "gcc.exe")
+    set(CMAKE_CXX_COMPILER "g++.exe")
 elseif(UNIX)
     set(CMAKE_C_COMPILER "/usr/bin/cc")
     set(CMAKE_CXX_COMPILER "/usr/bin/c++")
